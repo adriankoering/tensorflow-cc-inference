a0d71a7133cb11cf557b4cf768993382277c1469
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-22 21:21:52 +0100

    remove sudo from install script

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,7 @@
 TF_TYPE="cpu"
 OS="linux"
 TARGET_DIRECTORY= $PWD"/lib/"
-curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | sudo tar -C $TARGET_DIRECTORY -xz
+curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | tar -C $TARGET_DIRECTORY -xz
 
  export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY
  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY
