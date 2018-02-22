973d4e88155be4765884b0a3cfe84670fca369a0
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-22 22:04:42 +0100

    Add travis with sudo

--- travis/install.sh
+++ travis/install.sh
@@ -2,7 +2,7 @@ TF_TYPE="cpu"
 OS="linux"
 
 # this is a weird, but gets around reconfiguring include directories
-TARGET_DIRECTORY=$PWD"/include/tensorflow_inference"
+TARGET_DIRECTORY="/usr/local"
 
 curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | tar -C $TARGET_DIRECTORY -xz
 
