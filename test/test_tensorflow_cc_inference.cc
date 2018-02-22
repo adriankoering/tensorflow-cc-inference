11cb92f29ca0827be994ad0ca6ab139d7b98264b
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-22 23:13:41 +0100

    Update test and travis.yml

--- test/test_tensorflow_cc_inference.cc
+++ test/test_tensorflow_cc_inference.cc
@@ -1,31 +1,31 @@
 
 #include "tensorflow_inference/tensorflow_inference.h"
 #include <gtest/gtest.h>
-
+#include <string>
 using tensorflow_inference::TensorflowInference;
 
-TEST(TestOpenFile, DoesItRun) {
-  auto M = TensorflowInference("../test/graphs/x_times_2.pb", "x", "y");
+TEST(TestSimplegraph, DoesItRun) {
 
+  auto M = TensorflowInference("test/graphs/x_times_2.pb", "x", "y");
 
-    int64_t dims[1] = {1l, };
-    TF_Tensor* input_tensor = TF_AllocateTensor(TF_FLOAT,
-                                                dims, 1,
-                                                sizeof(float));
-    static_cast<float*>(TF_TensorData(input_tensor))[0] = 1.f;
+  int64_t dims[1] = {1l, };
+  TF_Tensor* input_tensor = TF_AllocateTensor(TF_FLOAT,
+                                              dims, 1,
+                                              sizeof(float));
+  static_cast<float*>(TF_TensorData(input_tensor))[0] = 1.f;
 
-    TF_Tensor* out = M(input_tensor);
+  TF_Tensor* out = M(input_tensor);
 
-    ASSERT_EQ(TF_TensorType(out), TF_FLOAT);
-    ASSERT_EQ(TF_NumDims(out), 1);
-    ASSERT_EQ(TF_Dim(out, 0), 1);
+  ASSERT_EQ(TF_TensorType(out), TF_FLOAT);
+  ASSERT_EQ(TF_NumDims(out), 1);
+  ASSERT_EQ(TF_Dim(out, 0), 1);
 
-    float* out_data = static_cast<float*>(TF_TensorData(out));
+  float* out_data = static_cast<float*>(TF_TensorData(out));
 
-    ASSERT_DOUBLE_EQ(out_data[0], 2);
+  ASSERT_DOUBLE_EQ(out_data[0], 2);
 }
 
 int main(int argc, char** argv){
-  testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
