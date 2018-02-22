979b4122fe65e83e21a443270c47f3f4c23ce4aa
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-22 21:42:34 +0100

    fix export paths

--- travis/install.sh
+++ travis/install.sh
@@ -4,8 +4,8 @@ TARGET_DIRECTORY="/usr/local/"
 
 curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | tar -C $TARGET_DIRECTORY -xz
 
-export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY
+export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib"
 
 
 echo $LIBRARY_PATH
