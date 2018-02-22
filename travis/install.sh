28adce9b75eaa756f0129583e132021175775ac8
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-22 22:13:30 +0100

    Add sudo back into install script

--- travis/install.sh
+++ travis/install.sh
@@ -4,7 +4,7 @@ OS="linux"
 # this is a weird, but gets around reconfiguring include directories
 TARGET_DIRECTORY="/usr/local"
 
-curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | tar -C $TARGET_DIRECTORY -xz
+curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | sudo tar -C $TARGET_DIRECTORY -xz
 
 export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
