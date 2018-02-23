2b68bcea3213a5200957a1c9d76d6b85bc616937
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-23 07:09:33 +0100

    Add test script to export LD_LIBRARY_PATH again

--- travis/install.sh
+++ travis/install.sh
@@ -1,13 +1,8 @@
 TF_TYPE="cpu"
 OS="linux"
 
-# this is a weird, but gets around reconfiguring include directories
 TARGET_DIRECTORY="/usr/local"
-
-curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | sudo tar -C $TARGET_DIRECTORY -xz
-
 export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
 
-
-sudo ldconfig $LIBRARY_PATH -v -n
+curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | sudo tar -C $TARGET_DIRECTORY -xz
---
+++ travis/test.sh
@@ -0,0 +1,6 @@
+
+TARGET_DIRECTORY="/usr/local"
+export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
+
+test/testrunner
