9e452c8f927fc2ed717398f8937363f8e0e25963
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-02-26 23:11:12 +0100

    Fix testrunner location

--- travis/test.sh
+++ travis/test.sh
@@ -3,4 +3,4 @@ TARGET_DIRECTORY="/usr/local"
 export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
 
-test/testrunner
+build/testrunner
