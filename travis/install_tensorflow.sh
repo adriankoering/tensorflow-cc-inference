c6cbb75603f99979b449c449dd6643d795487c98
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-03-01 23:12:18 +0100

    echo paths to .profile instead of exporting them

--- travis/install_tensorflow.sh
+++ travis/install_tensorflow.sh
@@ -2,7 +2,7 @@ TF_TYPE="cpu"
 OS="linux"
 
 TARGET_DIRECTORY="/usr/local"
-export LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY"/lib/"
+echo "LIBRARY_PATH=$LIBRARY_PATH:$TARGET_DIRECTORY/lib/" >> .profile
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TARGET_DIRECTORY/lib/" >> .profile
 
 curl -L "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-${OS}-x86_64-1.5.0.tar.gz" | sudo tar -C $TARGET_DIRECTORY -xz
