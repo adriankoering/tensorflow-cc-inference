f84d1bb3e1455840709a0d1af86e9ac9f17f57a4
Adrian Köring <30828061+adriankoering@users.noreply.github.com>
2018-03-20 20:10:58 +0100

    Add creation of frozen graph to example

--- example/keras_mnist.py
+++ example/keras_mnist.py
@@ -14,12 +14,18 @@ M = tf.keras.models.Model(x, y)
 # train the graph
 # M.fit(...)
 
-graph_def = tf.keras.backend.get_session().graph_def
+sess      = tf.keras.backend.get_session()
+graph_def = sess.graph.as_graph_def()
 print(graph_def) # get the input and output node name from here
 
+# 'freeze' the graph (convert variables into constants): allows to have both,
+# weights and the graph architecture in the same file
+frozen_graphdef = tf.graph_util.convert_variables_to_constants(
+    sess, graph_def, "Dense/Softmax")
+
 # save the graph
-tf.train.write_graph(graph_or_graph_def = graph_def,
-                     logdir = path, # absolute path
+tf.train.write_graph(graph_or_graph_def = frozen_graphdef,
+                     logdir = path, # requires absolute path
                      name = "mnist.pb",
                      as_text = False)
 # as_text must be false, because the C-API can only operate with binary graphs
